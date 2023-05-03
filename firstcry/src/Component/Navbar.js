c6e0ffd6d0dd328afd1951e8d0df3fc75a8946e4
Pre-11 <prernakumari7427@gmail.com>
2023-05-03 22:01:33 +0530

    navbar added

--- firstcry/src/Component/Navbar.js
+++ firstcry/src/Component/Navbar.js
@@ -3,7 +3,7 @@ import React from 'react'
 import { IoIosHeartEmpty} from "react-icons/io";
 import { MdOutlineLocationOn } from "react-icons/md";
 import { BsCartPlus } from "react-icons/bs";
-import { PhoneIcon, SearchIcon } from '@chakra-ui/icons'
+import { PhoneIcon, SearchIcon,ChevronDownIcon } from '@chakra-ui/icons'
 
 
 
@@ -74,19 +74,26 @@ function Navbar() {
                         <Text fontSize={15} fontWeight="400" >Cart</Text>
 
                     </Stack>
-
-
-
-
                 </Box>
 
 
-                <Box  >
-
-
-
-
-
+                <Box  display="flex" border='1px' borderColor='yellow' w="100%" h="50px"  bg="#ffd91c" >
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14} ml={45} p={3}>All CATEGORIES</Text>
+                  <ChevronDownIcon boxSize={6} mt={3}/>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>BOY FASHION</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>GIRL FASHION</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>FOOTWEAR</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>TOY</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>DIAPERING</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>GEAR</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>FEEDING</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>BATH</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>NURSERY</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>MOMS</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>HEALTH</Text>
+                  <Text  font-family="RobotoL"  fontWeight="450" fontSize={14}  p={3}>BOUTIQUES</Text>
+                  {/* <img src="https://cdn.fcglcdn.com/brainbees/images/n/club_logo.png" /> */}
+                  <img src="https://cdn.fcglcdn.com/brainbees/banners/FC-menu-carters-logo.jpg"/>
 
                 </Box>
             </Box>
